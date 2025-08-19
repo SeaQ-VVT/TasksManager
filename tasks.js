3f6ec54e6e619672f08058e870d1aad303ef21c4
YÊN <nguyenvanyen.el@gmail.com>
2025-08-19 12:32:26 +0700

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -18,7 +18,7 @@ import {
   updateDoc,
   serverTimestamp,
   deleteField,
-  increment
+  increment // Import thêm increment
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
