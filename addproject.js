c40c4417e89eb8a79187b73ca9dd0a2bff96ec84
YÊN <nguyenvanyen.el@gmail.com>
2025-08-19 15:58:47 +0700

    Update addproject.js

--- addproject.js
+++ addproject.js
@@ -158,7 +158,7 @@ function renderProject(docSnap) {
 
   const projectCard = document.createElement("div");
   projectCard.className =
-   "bg-white p-6 rounded-lg shadow-md border border-gray-200 transition-transform transform hover:scale-105 mb-4";
+   "bg-white p-6 rounded-lg shadow-md border border-gray-200 transition-transform transform hover:scale-110 mb-4";
   
   const createdAt = data.createdAt?.toDate ? data.createdAt.toDate().toLocaleString() : "-";
 
@@ -538,6 +538,7 @@ function setupSidebar() {
     });
   });
 }
+
 
 
 
